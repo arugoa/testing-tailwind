7bb6020ddf51d2802aa79f2d0e605d5e328c1711
arugoa <arihantjaingoa@gmail.com>
2025-07-16 11:04:06 -0700

    adding pwa to the page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './manifest.json';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest,
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      // switch to "true" to enable sw on development
+      devOptions: { enabled: true },
+      registerType: 'autoUpdate',
+      workbox: { globPatterns: ['**/*.{js,jsx,css,html}', '**/*.{svg,png,jpg,gif}', '**/**/*.{svg,png,jpg}'] },
+    }),
+  ],
 })
